1915852a9a19a6854db3359ce8e0207a6f12b0e5
hongs0312 <hongseongho300@gmail.com>
2024-05-09 11:18:01 +0900

    none

---
+++ data/InputData.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append("src")
+
+from ability.repository.CharacterRepository import CharacterRepository
+
+repo = CharacterRepository()
+
+file = open("character_information.txt", "r")
+
+while True:
+    line = file.readline()
+    if not line:
+        break
+
+    element = line[:-1].split('\t')
+
+    character_name = element[0]
+    character = repo.get_character(character_name)
+    if character is None:
+        character_id = repo.add_character(character_name)
+    else:
+        character_id = character["id"]
+
+    skill_type = element[1]
+    skill_name = element[2]
+    cooldown = float(element[3])
+    damage = element[4]
+    description = element[5]
+    image = element[6]
+    features = " / ".join(element[7:])
+
+    skill_id = repo.add_skill(skill_name, cooldown, damage, description, features, image)
+    repo.link_character_and_skill(character_id, skill_type, skill_id)
---
+++ data/check_file.py
@@ -0,0 +1,10 @@
+file = open("../../../../VIGIL/data/character_information.txt", "r")
+
+while True:
+    line = file.readline()
+    if not line:
+        break
+
+    element = line[:-1].split('\t')
+
+    print(element)
