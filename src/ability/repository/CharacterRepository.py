8dd7e3ea96029c2311ecd4521d99e7b8ae8e720f
hongs0312 <hongseongho300@gmail.com>
2024-05-10 13:05:47 +0900

    none

--- src/ability/repository/CharacterRepository.py
+++ src/ability/repository/CharacterRepository.py
@@ -1,5 +1,33 @@
 import pymysql
 
+character_information_create = \
+"CREATE TABLE `character_information` (`id` INT(10) UNSIGNED NOT NULL,\
+	`name` CHAR(128) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`primary` INT(10) UNSIGNED NOT NULL DEFAULT '0',\
+	`secondary` INT(10) UNSIGNED NOT NULL DEFAULT '0',\
+	`special` INT(10) UNSIGNED NOT NULL DEFAULT '0',\
+	`passive` INT(10) UNSIGNED NOT NULL DEFAULT '0',\
+	`strike` INT(10) UNSIGNED NOT NULL DEFAULT '0',\
+	PRIMARY KEY (`id`) USING BTREE\
+)\
+COLLATE='utf8mb4_general_ci'\
+ENGINE=InnoDB\
+;"
+
+skill_information_create = \
+"CREATE TABLE `skill_information` (\
+	`id` INT(10) UNSIGNED NOT NULL,\
+	`name` CHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`cooldown` CHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`damage` CHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`description` VARCHAR(1000) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`image` CHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	`feature` CHAR(200) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',\
+	PRIMARY KEY (`id`) USING BTREE\
+)\
+COLLATE='utf8mb4_general_ci'\
+ENGINE=InnoDB\
+;"
 
 class Connection:
     def __init__(self):
@@ -103,8 +131,22 @@ class CharacterRepository:
         cur.execute(query)
         conn.commit()
 
+    def make_table(self):
+        conn, cur = self.connection.get_connection()
+        cur.execute(character_information_create)
+        cur.execute(skill_information_create)
+        conn.commit()
+
+    def drop_table(self):
+        conn, cur = self.connection.get_connection()
+        query = "DROP TABLE character_information"
+        cur.execute(query)
+        query = "DROP TABLE skill_information"
+        cur.execute(query)
+        conn.commit()
+
 
 if __name__ == "__main__":
     repo = CharacterRepository()
 
-    print(repo.clear_repository())
+    repo.clear_repository()
